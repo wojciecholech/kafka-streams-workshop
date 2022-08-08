240d6ae2b60209b9eb862f7b02e0de8241f70deb
Wojciech Olech <wojciech.olech@sinch.com>
2022-08-08 13:32:21 +0200

    Init

---
+++ src/main/java/com/sinch/kafka/KafkaStreamsWorkshopApplication.java
@@ -0,0 +1,13 @@
+package com.sinch.kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaStreamsWorkshopApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(KafkaStreamsWorkshopApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/sinch/kafka/KafkaStreamsWorkshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sinch.kafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaStreamsWorkshopApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
